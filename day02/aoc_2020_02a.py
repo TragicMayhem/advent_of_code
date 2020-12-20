b7881758675702abc8262dafafbe0ac86e073b49
Alex Long <alexlong26@gmail.com>
2020-12-20 20:55:35 +0000

    day02 initial version

---
+++ day02/aoc_2020_02a.py
@@ -0,0 +1,30 @@
+import re
+
+print("Advent of Code 2020 - Day 2 part 1")
+
+# Input file is a list of policy and passwords
+# n-m : passwordstring
+# How many on the input file are valid
+
+valid_pwd = 0
+
+handle = open('test.txt', 'r')
+#handle = open('input.txt', 'r')
+lines = handle.readlines()
+handle.close()
+
+for line in lines:
+  chk = re.match(r"(\d+)-(\d+) ([a-z]): ([a-z]+)", line)
+  lower, upper, letter, password = chk.group(1, 2, 3, 4)
+  print(lower, upper, letter, password)
+  lower = int(lower)
+  upper = int(upper)
+  occurances = password.count(letter)
+  if (lower <= occurances <= upper):
+      valid_pwd += 1
+
+print(valid_pwd)
+
+  
+
+
