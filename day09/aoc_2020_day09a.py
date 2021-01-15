d9d8acf0239992503f79d4a2eb949030cc1d85fe
Alex Long <alexlong26@gmail.com>
2021-01-15 10:34:25 +0000

    misc updates

--- day09/aoc_2020_day09a.py
+++ day09/aoc_2020_day09a.py
@@ -11,6 +11,7 @@ filename = 'input.txt'   # 393911906
 # 5 for test data, 25 for input
 preamble_len = 25
 
+
 def canSum(targetsum, numbers, memo=None):
   # print("__call:", targetsum, numbers, memo)
   if memo == None: memo = {}
@@ -39,19 +40,5 @@ with open(dirpath + filename, 'r') as file:
   for x in range(preamble_len, len(lst)):
       # print(x, "lst[x]", lst[x], "pass:", canSum(lst[x], lst[x - preamble_len :x]))
       if not canSum(lst[x], lst[x - preamble_len :x]):
-        print(x, "lst[x]", lst[x], "pass:", canSum(lst[x], lst[x - preamble_len :x]))
+        print("lst[",x, "] =", lst[x], "  pass:", canSum(lst[x], lst[x - preamble_len :x]))
         break
-
-  
-  # pprint(lst)
-
-  # print("TESTS\n")
-  # print("\n-----\ncanSum(lst[preamble_len], lst[0:preamble_len])",lst[preamble_len], lst[0:preamble_len])
-  # print(canSum(lst[preamble_len], lst[0:preamble_len]))
-
-  # print("\n-----\ncanSum(lst[preamble_len + 1], lst[0 + 1:preamble_len + 1])",lst[preamble_len + 1], lst[0 + 1:preamble_len + 1])
-  # print(canSum(lst[preamble_len + 1], lst[0 + 1:preamble_len + 1]))
-
-  # print("\n-----\ncanSum(lst[preamble_len + 2], lst[0 + 2:preamble_len + 2])",lst[preamble_len + 2], lst[0 + 2:preamble_len + 2])
-  # print(canSum(lst[preamble_len + 2], lst[0 + 2:preamble_len + 2]))
-  
