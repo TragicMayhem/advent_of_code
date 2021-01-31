05437844e75d7714a71f11eed150bb0343d6257f
Alex Long <alexlong26@gmail.com>
2021-01-31 14:03:13 +0000

    day24 done (ish?)

--- aoc_2015/day24/aoc_2015_24pt1.py
+++ aoc_2015/day24/aoc_2015_24pt1.py
@@ -8,11 +8,18 @@ from pprint import pprint
 print("Advent of Code 2015 - Day 24 part 1")
 print(".......patient, its running. some comments left in to show where it is up to.")
 print(".......it is A LOT of combinations and inefficient code.... but it got the right answer")
+
 dirpath = sys.path[0] + '\\'
 
 filename = 'test.txt'  # Num parcels 10 with total weight of 60 Group target weight is 20.0  > 99
-filename = 'input.txt'  # Num parcels 28 with total weight of 1548 Group target weight is 516.0  > 11266889531  (TAKES AGES!)
+filename = 'input.txt'  # 
+# PART1 with 3 groups = Num parcels 28 with total weight of 1548 Group target weight is 516.0  > 11266889531  (TAKES AGES!)
+# PART2 with 4 groups = Num parcels 28 with total weight of 1548 Group target weight is 387.0  >  (TAKES AGES!)
 
+target_groups_part1 = 3
+target_groups_part2 = 4
+
+number_groups = target_groups_part2
 answers = []
 
 with open(dirpath + filename, 'r') as file:
@@ -20,16 +27,17 @@ with open(dirpath + filename, 'r') as file:
   parcels = [int(d) for d in data]
   num_parcels = len(parcels)
   total_weight = sum(parcels)
-  target_weight = total_weight / 3
+  target_weight = total_weight / number_groups
 
   print("\nParcels:", parcels)
   print('Num parcels',num_parcels,'with total weight of',total_weight,'Group target weight is', target_weight, "\n")
 
   parcels.sort(reverse=True)
 
-  for g1 in range(4, len(parcels) // 4):
+  for g1 in range(4, len(parcels) // number_groups):
     print("working....", g1)
     # group1_combis = itertools.combinations(parcels, g1)
+
     for c1 in itertools.combinations(parcels, g1):
       left_after_group1 = [x for x in parcels if x not in c1]  # Calculate parcel weights left (not used)  (Possible Group 2 and Group3)
 
