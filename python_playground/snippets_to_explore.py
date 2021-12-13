0961cec4abd82f5148431c71e680f3665711618a
Alex L <alexlong26@gmail.com>
2021-12-13 21:31:34 +0000

    misc tidy up

--- python_playground/snippets_to_explore.py
+++
@@ -1,6 +0,0 @@
-#https://github.com/tomp/AOC-2016/blob/master/day3/day3.py
-
-vals = list([row[0] for row in triples] + 
-             [row[1] for row in triples] + 
-             [row[2] for row in triples])
-triples2 = zip(vals[0::3], vals[1::3], vals[2::3])
