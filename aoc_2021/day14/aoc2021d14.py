8518e26c24c1630142c9fc9f9a55c63799ae7a28
Alex L <alexlong26@gmail.com>
2021-12-14 20:47:03 +0000

    minor reorg

--- aoc_2021/day14/aoc2021d14.py
+++ aoc_2021/day14/aoc2021d14.py
@@ -7,8 +7,8 @@ from collections import defaultdict
 from collections import Counter
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / 'input.txt'  # 2447 / 
-input_test = script_path / 'test.txt'  # 1588
+input = script_path / 'input.txt'  # 2447 / 3018019237563
+input_test = script_path / 'test.txt'  # 1588 / 2188189693529
 
 
 def parse(puzzle_input):
@@ -62,26 +62,6 @@ def part1(polymer, insertion_rules, steps=10):
     return answer
 
 
-def loopPolymer(polymer, rules):
-    c=0
-    next_polymer=''
-
-    while c < len(polymer)-1:
-        check=polymer[c:c+1]
-
-        if check in rules.keys():
-            insertion = check[0] + rules[check]
-        else:
-            insertion = check[0]
-        
-        c+=1
-        next_polymer = next_polymer + insertion
-
-    next_polymer = next_polymer + polymer[-1]
-
-    return next_polymer
-
-
 def next2Char(polymer):
 
     for i in range(len(polymer)-1):
@@ -90,7 +70,6 @@ def next2Char(polymer):
 
 def part2(polymer, insertion_rules, steps=10):
     """Solve part 2"""   
-    print('PART 2')
     current_polymer = polymer
 
     last_char = current_polymer[-1]
@@ -100,61 +79,56 @@ def part2(polymer, insertion_rules, steps=10):
     for k,v in insertion_rules.items():
         polyCombis[k] = (k[0] + v, v + k[1])
     
+    # print(polyCombis) # defaultdict(None, {'CH': ('CB', 'BH'), 'HH': ('HN', 'NH'),
     
-    print(polyCombis) # defaultdict(None, {'CH': ('CB', 'BH'), 'HH': ('HN', 'NH'),
-    
-    # tally = Counter(current_polymer)
     # a = ("John", "Charles", "Mike")
     # x = zip(a, a[1:])
     # ('John', 'Charles'), ('Charles', 'Mike'))
 
-    polyCombiCount = defaultdict(int)
+    polymerCounts = defaultdict(int)
 
     for pair in next2Char(polymer):
-        polyCombiCount[pair] += 1
+        polymerCounts[pair] += 1
        
-    print(polyCombiCount) # defaultdict(<class 'int'>, {'NN': 1, 'NC': 1, 'CB': 1})
+    # print(polymerCounts) # defaultdict(<class 'int'>, {'NN': 1, 'NC': 1, 'CB': 1})
 
     for i in range(steps):
-        print('\nstep', i)
+        # print('\nstep', i)
 
         nextPolymerCounts = defaultdict(int)
 
-        for pPair in polyCombiCount:
+        for pPair in polymerCounts:
             newCombi = polyCombis.get(pPair)
-            print('pPair',pPair)
-            print('newCombi',newCombi)
+            # print('pPair',pPair
+            # print('newCombi',newCombi)
 
             if newCombi:
-                count = polyCombiCount[pPair]
-                print(pPair, newCombi, count)
+                count = polymerCounts[pPair]
+                # print(pPair, newCombi, count)
 
                 nextPolymerCounts[newCombi[0]] += count
                 nextPolymerCounts[newCombi[1]] += count
             else:
                 nextPolymerCounts[pPair] = polyCombis[pPair]
             
-            print(nextPolymerCounts)
+            # print(nextPolymerCounts)
 
-        polyCombiCount = nextPolymerCounts
+        polymerCounts = nextPolymerCounts
 
 
-# dont double count chars as pairs - think about last one?
+    # TODO: dont double count chars as pairs - think about last one?
     char_count = defaultdict(int)
-    for k, n in polyCombiCount.items():
+    for k, n in polymerCounts.items():
         char_count[k[0]] += n
-    
-    print(char_count)
 
     char_count[last_char] += 1
-    print(char_count)
+    # print(char_count)
 
-    # for k, n in polyCombiCount.items():
+
     # most_freq_char = msg_char_counter.most_common()[0][0]
     # least_freq_char = msg_char_counter.most_common()[-1][0]
 
     answer = max(char_count.values())- min(char_count.values())
-
 
     return answer
 
