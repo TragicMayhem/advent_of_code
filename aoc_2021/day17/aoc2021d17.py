2466c695a81718d77640960351ab0174c0c0cc25
Alex L <alexlong26@gmail.com>
2021-12-17 14:27:39 +0000

    d17 done easy and hard ways

--- aoc_2021/day17/aoc2021d17.py
+++ aoc_2021/day17/aoc2021d17.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2021/day/x
 
+from os import curdir
 import pathlib
 import time
 import re
@@ -22,85 +23,169 @@ def parse(puzzle_input):
     with open(puzzle_input, 'r') as file:
         line = file.readline()
         res = re.search(re_pattern,line)
-        # print(len(res.groups()))
 
         if res:
-            # x1, x2, y1, y2 = res.group(1,2,3,4)
-            # print(x1,x2,y1,y2)
-            target = [int(x) for x in res.groups()]
+            target = [int(x) for x in res.groups()]       #  map?
       
     return tuple(target)
 
 
-# def inc_x(x_low, x_high):
-
-#     diff = x_high - x_low
+def delta_change(dx, dy, step):
     
-#     while x_low <= val <= x_high:            
-#         for i in range(target, 0, -1):
-#             val = val - 1
-#             print(i, val)
+    new_dx = dx - step if dx > 0 else 0
+    new_dy = dy - step
+
+    return (new_dx, new_dy)
+
+
+# def new_location(cx, cy, dx, dy):
+    
+#     new_dx = dx - 1 if dx > 0 else 0
+#     new_dy = dy - 1
+    
+#     new_x = cx + dx
+#     new_y = cy + dy
+
+#     return (new_x, new_y)
 
 
 def part1(data):
     """Solve part 1""" 
     
     (target_x1, target_x2, target_y1, target_y2) = data
-    print(data)
+   
+    # target_xsize = abs(target_x1 - target_x2)
+    # target_ysize = abs(target_y1 - target_y2)
+    # print("target size",target_xsize, target_ysize)
 
-    start = (0,0)
+    # y2 wrong: print(-73 * (-73 + 1) / 2)  # not 2628
+    # y1 right: print(-98 * (-98 + 1) / 2)  # 4753
+    ans = int(target_y1 * (target_y1 + 1) / 2)
 
-    '''
-    loop steps
-    each one 
-        x = x + (x-1 if x>0 or x+1<0 towards 0)  << how many steps to get to zero?
-        y = y + (y-1) because of gravity
-    '''
-
-    target_xsize = abs(target_x1 - target_x2)
-    target_ysize = abs(target_y1 - target_y2)
-    print(target_xsize, target_ysize)
-
-    winning_results = []
-    xcurr=0 
-    ycurr=0
-    if target_x1 <= xcurr <= target_x2:
-        print("in x target")
-    if target_y1 <= ycurr <= target_y2:  # careful of negative, might need to check this
-        print("in y target")
-
-    # inc_x(target_xsize)
+    return ans
 
 
-    # how many steps to get x over to the target
-    x_steps_to_target = 0
-    val=0
+# copy and paste first attempt to work out logic and then rationalize 
+# (trial and error with a little math!)
 
+    # target_xsize = abs(target_x1 - target_x2)
+    # target_ysize = abs(target_y1 - target_y2)
+    # print("target size",target_xsize, target_ysize)
+ 
+            # while tracking and step_count<50:
 
-    for i in range(0,target_xsize):
-        pass
-        # val = 1    
-        # x_steps_to_target += i target_xsize 
+            #     dx, dy = delta_change(dx, dy, step_count)
 
+            #     x_pos, y_pos = (x_pos + dx, y_pos + dy)
+            #     print("current", x_pos, y_pos)
 
+            #     if y_pos > max_y:   #  track highest y value
+            #         print("New heights", y_pos)
+            #         max_y = y_pos
 
-    for x in range(0,target_xsize+1):
-        # fine towards target and getting slower
-        print(x)
+            #     if x_pos == 0 and x_pos < target_x1:
+            #         print("Falling short.....")
+            #         tracking=False
+            #         break
 
-        for y in range(0,target_ysize):  #not right for parabola
-            print(x,y)  
+            #     if y_pos > target_y2:
+            #         print("Below target.....")
+            #         tracking=False
+            #         break
 
+            #     if x_pos > target_x2 and y_pos < target_y1:
+            #         print("Overshot.....")
+            #         tracking=False
+            #         break
 
+            #     if target_x1 <= x_pos <= target_x2:
+            #         print("In X zone")
+                    
+            #         if (y_pos > target_y2):
+            #             print("Undershot.....")
+            #             tracking=False
+            #             break
 
-    return 1
+            #     if target_x1 <= x_pos <= target_x2 and \
+            #         target_y1 <= y_pos <= target_y2:  # careful of negative, might need to check this
+            #         print("--- in target ---")
+            #         tracking = True
+            #         winning_results.append(((x,y),max_y))
+            #         break
 
+            #     if x_pos < target_x1 and y_pos > target_y1:
+            #         pass
+
+            #     if abs(y_pos) > abs(target_y2):
+            #         pass
+
+            #     if abs(y_pos) < target_y1 and x_pos > target_x2:
+            #         pass
+
+            #     step_count += 1
 
 def part2(data):
     """Solve part 2"""   
    
-    return 1
- 
+    (target_x1, target_x2, target_y1, target_y2) = data
+    # print("Data",data)
+
+    start = (0,0)
+    winning_results = []
+
+    # These two loops (x,y) are trying the combincations of trajectory
+    for dx in range(0, target_x2+1):
+        # fire towards target and getting slower (x)
+        # print("x:", dx)
+
+        max_y = 0
+        
+        # for y in range(0, target_y1*10):  
+        for dy in range(target_y1, -target_y1):  
+            # not right for parabola - do we need to start at target y? negatives?
+            # fire up (or down?) and gravity affects pulling down
+            # print("Start combination x,y:", dx, dy)
+
+            # track the y.  if going up good, if going down stop.  cause got to the highest. how?
+            step_count = 1
+            x_pos, y_pos = start
+            
+            # prev_y = 0
+            combi = (dx, dy)  # initial delta
+            current_dx = dx
+            current_dy = dy
+            
+
+            while x_pos <= target_x2 and y_pos >= target_y1: # or y2?
+                # print(current_dx, current_dy)
+
+                if y_pos > max_y:   #  track highest y value
+                    # print("New heights", y_pos)
+                    max_y = y_pos
+
+                if target_x1 <= x_pos <= target_x2 and \
+                    target_y1 <= y_pos <= target_y2:  # careful of negative, might need to check this
+                    # print("--- in target ---")
+                    winning_results.append(((dx,dy),max_y))
+                    break
+
+                x_pos += current_dx
+                y_pos += current_dy
+
+                current_dy -= 1 
+                current_dx = current_dx - 1 if current_dx > 0 else 0
+           
+
+
+    # print("Winning vectors\n",winning_results)
+
+    how_many_winning_vectors = len(winning_results)
+
+    #Answers for Part 1 without using the simple math formula (thats not fool proof)
+    whats_the_highest_y = max(winning_results,key=lambda item:item[1])[1]
+    whats_the_vector_for_highest_y = max(winning_results,key=lambda item:item[1])[0]
+    
+    return how_many_winning_vectors
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input"""
@@ -135,8 +220,8 @@ if __name__ == "__main__":    # print()
 
     runAllTests()
 
-    # solutions = solve(input)
-    # print('\nAOC')
-    # print(f"Solution 1: {str(solutions[0])} in {solutions[2][1]-solutions[2][0]:.4f}s")
-    # print(f"Solution 2: {str(solutions[1])} in {solutions[2][2]-solutions[2][1]:.4f}s")
-    # print(f"\nExecution total: {solutions[2][-1]-solutions[2][0]:.4f} seconds")
+    solutions = solve(input)
+    print('\nAOC')
+    print(f"Solution 1: {str(solutions[0])} in {solutions[2][1]-solutions[2][0]:.4f}s")
+    print(f"Solution 2: {str(solutions[1])} in {solutions[2][2]-solutions[2][1]:.4f}s")
+    print(f"\nExecution total: {solutions[2][-1]-solutions[2][0]:.4f} seconds")
