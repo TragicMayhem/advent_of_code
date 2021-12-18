9fa77e4a1a52fe2e32b58e9add5945435cfae6ef
Alex L <alexlong26@gmail.com>
2021-12-18 23:21:01 +0000

    d18 experiment correct needs massive tidyup

--- aoc_2021/day18/experiments/exp_using_re 2 fixing for over tens.py
+++ aoc_2021/day18/experiments/exp_using_re 2 fixing for over tens.py
@@ -1,23 +1,16 @@
 import pathlib
 import re
-from typing import final
-import ast
-from itertools import permutations
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / 'input.txt'  # 3488 / 4583
-input_test2 = script_path / 'test2.txt'  # 3488 / 3805
-input_test3 = script_path / 'test3.txt'  # 4140 / 3993
-
+input_test = script_path / 'test2.txt'  # 
 
 def parse(puzzle_input):
     """Parse input """
 
     with open(puzzle_input, 'r') as file:
         lines = file.read().replace(' ','').split('\n')
-        # print(lines)
-    return lines
 
+    return lines
 
 ##### PATTERNS #####
 two_digits_pattern = re.compile('(\d{2})')
@@ -39,11 +32,19 @@ def explode_node(data_in, current_pos):
     # add and replace those numbers at those positions
     # replace this one with 0
 
+    # num_explode_to_left = data_in[current_pos]
+    # num_explode_to_right = data_in[current_pos+2]
+
     pair = find_digit_pair.search(data_in[current_pos:])
     if pair:
         num_explode_to_left, num_explode_to_right = pair.groups()
         start, end = pair.span()
-    # print("  exploding at pos",current_pos, 'nums', num_explode_to_left, num_explode_to_right, 'pos',start,end)
+    print("  exploding at pos",current_pos, 'nums', num_explode_to_left, num_explode_to_right, 'pos',start,end)
+
+    # to_the_left = data_in[:current_pos] if int(num_explode_to_left) < 10 else data_in[current_pos-1:]
+    # to_the_right = data_in[current_pos+3:] if int(num_explode_to_right) < 10 else data_in[current_pos+4:]
+
+    # len(left number) +','
 
     to_the_left = data_in[:current_pos]
     to_the_right = data_in[current_pos+end:]
@@ -67,7 +68,9 @@ def explode_node(data_in, current_pos):
         # cant find number to the right so need to keep string and remove first bracket
         new_right = to_the_right[1:]
 
-    # print('  new left:', new_left,' new right:',new_right)
+    print('  new left:', new_left,' new right:',new_right)
+
+    # data_out = data_in[:current_pos-1] + ['0'] + data_in[current_pos+3:] 
     data_out = new_left + '0' + new_right
     return data_out
 
@@ -90,109 +93,45 @@ def collapse_next(data_in):
     big_numbers = two_digits_pattern.search(data_in)
 
     if big_numbers:
-        # print('  splitting',big_numbers.group(0))
+        # start, end = big_numbers.span()
+        # data_out = re.sub(r'(\d{2})', make_new_split(big_numbers.group(0)), data_out, count=1)
+        print('  splitting',big_numbers.group(0))
         return re.sub(two_digits_pattern, make_new_split(int(big_numbers.group(0))), data_in, count=1)
 
     return False
 
 
 def reduce_input(data_in):
+
     answer = data_in.pop(0)  # Get initial answer as first element
     
     for i, elem in enumerate(data_in):
         answer = '[' + answer +',' + elem + ']'
 
-        while True: # and c < 25:
-            # print('\nprocess', answer)
+        c = 0
+
+        while True and c < 20:
+            print('\nprocess', answer)
             tmp_answer = collapse_next(answer)
-            # print(tmp_answer)
+            print(tmp_answer)
 
             if tmp_answer == False:
+                print("breaking")
                 break
             else:
                 answer = tmp_answer
+            c+=1
+        
+        print(' >>>> end',i,' ', answer)
+        break
+    print()
+    print('ANSWER\n')  
+    print(answer)
 
-    # print()
-    # print('final reduction')  
-    # print(answer)
-    return answer
-
-
-def calc_mag(reduced_data):
-    
-    if isinstance(reduced_data,list):
-        left = reduced_data[0]
-        right = reduced_data[1]
-
-        return 3*calc_mag(left) + 2*calc_mag(right)
-    else:
-        return reduced_data
-
-
-input_data = parse(input_test3)
+input_data = parse(input_test)
 print(input_data)
 print()
-final_reduction = reduce_input(input_data)
-
-final_list = ast.literal_eval(final_reduction)
-print(final_list)
-mag=calc_mag(final_list)
-print(mag)
-
-print('------------------')
-
-t1 = ['[[[5,[2,8]],4],[5,[[9,9],0]]]', '[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]']
-t2 = ['[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]', '[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]']
-t_fr = reduce_input(t2)
-print(t_fr)
-print(calc_mag(ast.literal_eval(t_fr)))
-
-print('<<<<<<<<<<<<<<>>>>>>>>>>>>>')
-
-input_data = parse(input)
-print(input_data)
-
-final_reduction_perms = []
-possible_magnitudes = []
-perms = []
-results = []
-
-for p in permutations(input_data, 2): # All permutations of 2 fish numbers
-    perms.append(p)
-    fr = reduce_input(list(p))
-    final_reduction_perms.append(fr)
-    mag = calc_mag(ast.literal_eval(fr))
-    possible_magnitudes.append(mag)
-    results.append([p,fr,mag])
-    
-
-# print(perms)
-print('________________')
-print(perms[0])
-print(perms[1])
-print(len(perms))
-
-
-# for p in perms:
-#     fr = reduce_input(list(p))
-#     final_reduction_perms.append(fr)
-#     mag = calc_mag(ast.literal_eval(fr))
-#     possible_magnitudes.append(mag)
-#     results.append([fr,mag])
-
-# print(results)
-print()
-print(final_reduction_perms[0])
-print(final_reduction_perms[1])
-print(len(final_reduction_perms))
-
-possible_magnitudes.sort(reverse=True)
-print(possible_magnitudes)
-print(possible_magnitudes[0:2])
-
-
-
-
+reduce_input(input_data)
 
 print('########## AD HOC ###########')
 test1 = '[[1, 2], [[1, 2], 3], [9, [8, 7]], [[1, 9], [8, 5]], [[[[1, 2], [3, 4]], [[5, 6], [7, 8]]], 9]]'
