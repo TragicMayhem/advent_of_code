cbb4af63e4691bd52d50c1391da383ee30100681
Alex L <alexlong26@gmail.com>
2021-12-22 19:34:20 +0000

    updates 2015d18 complete

--- aoc_2015/day18/aoc2015d18.py
+++ aoc_2015/day18/aoc2015d18.py
@@ -7,9 +7,9 @@ import copy
 from pprint import pprint
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / 'input.txt'  # 821 / 
-input_test = script_path / 'test_part1.txt'  # 4
-input_test2 = script_path / 'test_part2.txt'  #
+input = script_path / 'input.txt'  # 821 / 886
+input_test = script_path / 'input_test.txt'  # 4 / 17
+# Part 2 same test input just lock the corners?
 
 
 def parse(puzzle_input):
@@ -19,19 +19,10 @@ def parse(puzzle_input):
     return lights_grid
 
 
-## needs to be for max grid size, pass in grid len?
-def corner(x, y): return (x,y) in [(0,0),(0,99),(99,0),(99,99)]
-
-
-def lockCorners():
-
-    return 1
-
-
-def get_coords8d(r, c, h, w):
+def get_coords8d(r, c, size):
     for delta_r, delta_c in ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)):
         rr, cc = (r + delta_r, c + delta_c)
-        if 0 <= rr < h and 0 <= cc < w:
+        if 0 <= rr < size and 0 <= cc < size:
             yield (rr, cc)
 
 
@@ -52,7 +43,7 @@ def next_state(grid, grid_size, posx, posy):
     '''
     current_status = grid[posx][posy]
 
-    lights_on = sum(filter(None, [grid[x][y] for x,y in get_coords8d(posx, posy, grid_size, grid_size)]))
+    lights_on = sum(filter(None, [grid[x][y] for x,y in get_coords8d(posx, posy, grid_size)]))
 
     if current_status and 2 <= lights_on <= 3:
         return True
@@ -71,28 +62,87 @@ def part1(lights, steps=100):
     # print("START lights on:", sum([sum(d) for d in lights]))
 
     for step in range(steps):
-        new_state_grid = []
-
+        new_state_grid = [None] * grid_size
         for i in range(grid_size):
             tmp = []
             for j in range(grid_size):
                 tmp.append(next_state(lights, grid_size, i, j)) 
-            
-            new_state_grid.append(tmp)
+            new_state_grid[i] = tmp
+        # pprint(new_state_grid)
 
         # print(f"State {step+1} has {sum([sum(d) for d in new_state_grid])} lights on")
-        # lights = copy.deepcopy(new_state_grid)
-        lights = new_state_grid
+        lights = copy.deepcopy(new_state_grid)
 
     answer = sum([sum(d) for d in lights])
 
     return answer
 
 
-def part2(data, steps=100):
-    """Solve part 2"""
+## PART 2 ##
 
-    return 1
+
+def change_state(lights_on, size, coords):
+    '''
+      Updates lights based on neighbours status
+      On: Stays on if 2 or 3 neighbours on else off.  
+      Off: Turns on if 3 neighbours on, else stays off
+      Edges (assume off)
+
+      Parameters:
+        x (int): row of the grid
+        y (int): column of the grid
+        [][] (bool): Grid with True/False
+
+      Returns:
+        next state (Bool):  True (on) False (off)
+    '''
+    current_status = coords in lights_on
+    (x,y) = coords
+
+    if coords in ({(0,0),(0,size-1),(size-1,0),(size-1,size-1)}):
+        # print('corner keeping on')
+        return True
+
+    how_many_on_around = sum(filter(None, [(x,y) in lights_on  for x,y in get_coords8d(x, y, size)]))
+
+    if current_status and 2 <= how_many_on_around <= 3:  # ON already with 2 to 3 around = stays ON
+        return True
+
+    if not current_status and how_many_on_around == 3:  # OFF already with exactly 3 on around = turns ON
+        return True
+
+    return False
+
+
+def part2(lights, steps=100):
+    """Solve part 2"""
+     
+    grid_size = len(lights)
+    
+    lights_on = set()
+    corners = {(0,0),(0,grid_size-1),(grid_size-1,0),(grid_size-1,grid_size-1)}
+    
+    # build initial for now
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if lights[i][j]:
+                lights_on.add((i,j))
+    lights_on.update(corners)  # Make sure corners are on
+
+    for step in range(steps):
+        tmp_lights = lights_on.copy()
+        
+        for row in range(grid_size):
+            for col in range(grid_size):
+                if change_state(lights_on, grid_size, (row,col)):
+                    tmp_lights.add((row,col))
+                else:
+                    tmp_lights.discard((row,col))
+
+        # print(f"After step {step+1} has {len(tmp_lights)} lights on")
+        lights_on = tmp_lights.copy()
+
+    return len(lights_on)
 
 
 def solve(puzzle_input):
@@ -115,7 +165,7 @@ def runAllTests():
     print("\nTests\n")
     data = parse(input_test)
     test_solution1 = part1(data, 4)
-    data = parse(input_test2)
+    data = parse(input_test)
     test_solution2 = part2(data, 5)
 
     print(f'Test1.  Part1: {test_solution1} Part 2: {test_solution2}')
@@ -130,5 +180,3 @@ if __name__ == "__main__":    # print()
     print(f"Solution 1: {str(solutions[0])} in {solutions[2][1]-solutions[2][0]:.4f}s")
     print(f"Solution 2: {str(solutions[1])} in {solutions[2][2]-solutions[2][1]:.4f}s")
     print(f"\nExecution total: {solutions[2][-1]-solutions[2][0]:.4f} seconds")
-# 
-# def corner(x, y): return (x,y) in [(0,0),(0,99),(99,0),(99,99)]
