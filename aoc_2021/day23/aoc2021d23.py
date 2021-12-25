1e1cf4872c8668fd7b7e4185b2eec9525b6e9d2a
Alex L <alexlong26@gmail.com>
2021-12-25 15:22:30 +0000

    d23-24 WIP d25 done

--- aoc_2021/day23/aoc2021d23.py
+++ aoc_2021/day23/aoc2021d23.py
@@ -7,13 +7,54 @@ script_path = pathlib.Path(__file__).parent
 input = script_path / 'input.txt'  # 
 input_test = script_path / 'test.txt'  # 
 
+'''
+NOTES
+hallway is 11 long - actually 7 if they cannot stop in the space outside room
+4 rooms each 2 spaces
+rooms L-R target is to hold A-B-C-D
+
+7 holding spaces
+8 spaces in rooms
+8 target spaces
+
+A cost 1
+B cost 10
+C cost 100
+D cost 1000 
+
+need to know each status for the rules
+- locked hallway (after moving there, then waiting for their room)
+- not in target
+- still in start room
+- hasnt moved yet, allowed to stay in rooms
+- once in a hallway, wont move unless their room is free (target room)
+- cant wait in space outside the rooms (effectively blocked?)
+- cant go into a room if there is one already not in final place
+- need to count number of moves with each. unique id
+
+- if pos in source room, is low(1), and not target room,     need to move before target can move in 
+
+'''
+
+target_layout = ('AA','BB','CC','DD')
+hallway = '...........'
+valid_hallway_idn = (0,1,3,4,7,9,10)   # Not in the spaces outside the rooms
+
+
+COSTS = {
+    'A' : 1,
+    'B' : 10,
+    'C' : 100,
+    'D' : 1000
+}
+
 
 def parse(puzzle_input):
     """Parse input """
 
     with open(puzzle_input, 'r') as file:
-      data = file.read().split('\n')
-    #   data = [d.split(' | ') for d in file.read().split('\n')]
+        data = file.read().split('\n')
+    #    data = [d.split(' | ') for d in file.read().split('\n')]
 
     return data
 
--- aoc_2021/day25/aoc2021d25.py
+++ aoc_2021/day25/aoc2021d25.py
@@ -12,22 +12,63 @@ def parse(puzzle_input):
     """Parse input """
 
     with open(puzzle_input, 'r') as file:
-      data = file.read().split('\n')
-    #   data = [d.split(' | ') for d in file.read().split('\n')]
+        data = [list(d) for d in file.read().split('\n')]
 
     return data
 
 
-def part1(data):
+EAST = '>'
+SOUTH = 'v'
+EMPTY = '.'
+
+
+def part1(grid):
     """Solve part 1""" 
-                  
-    return 1
 
+    y_size, x_size  = len(grid), len(grid[0])
+    num_steps = 1
 
-def part2(data):
-    """Solve part 2"""   
-   
-    return 1
+    # Check the grid for the EAST then the SOUTH but don't change anything.
+    # Just log the changes to apply later, if not then need to copy the grid (unknown size)
+    # every iteration (which is inefficient). If we have the grid, check for all the changes
+    # then apply them after, before checking for static shape or then restarting the checks.
+
+    while True:
+        moves_to_make = []  # Reset to process EAST changes
+
+        for y in range(y_size):
+            for x in range(x_size):
+                next_column = (x + 1) % x_size   # Use mod to capture the remainders and deal with the wrap around.
+
+                if grid[y][x] == EAST and grid[y][next_column] == EMPTY:
+                    moves_to_make.append((y, x, next_column))
+
+        # process ALL the EAST moves now before looking at SOUTH
+        for y, x, next_column in moves_to_make:
+            grid[y][x]    = EMPTY
+            grid[y][next_column] = EAST
+
+        in_east_locked_position = not moves_to_make   # If there is something on the list, then its True.
+
+        moves_to_make = []  # Reset for the SOUTH changes
+
+        for y in range(y_size):
+            for x in range(x_size):
+                next_row = (y + 1) % y_size
+
+                if grid[y][x] == SOUTH and grid[next_row][x] == EMPTY:
+                    moves_to_make.append((y, x, next_row))
+
+        if in_east_locked_position and not moves_to_make:  # if east locked, and now south no moves then done
+            break
+
+        for y, x, next_row in moves_to_make:
+            grid[y][x]    = EMPTY
+            grid[next_row][x] = SOUTH
+
+        num_steps +=1
+
+    return num_steps            
  
 
 def solve(puzzle_input):
@@ -39,24 +80,21 @@ def solve(puzzle_input):
     times.append(time.perf_counter())
     solution1 = part1(data)
     times.append(time.perf_counter())
-    solution2 = part2(data)
-    times.append(time.perf_counter())
-    
-    return solution1, solution2, times
+
+    return solution1, times
 
 
 def runTest(test_file):
     data = parse(test_file)
     test_solution1 = part1(data)
-    test_solution2 = part2(data)
-    return test_solution1, test_solution2
+    return test_solution1
 
 
 def runAllTests():
     
     print("Tests")
-    a, b  = runTest(input_test)
-    print(f'Test1.  Part1: {a} Part 2: {b}')
+    a  = runTest(input_test)
+    print(f'Test1.  Part1: {a}')
 
 
 if __name__ == "__main__":    # print()
@@ -65,6 +103,4 @@ if __name__ == "__main__":    # print()
 
     solutions = solve(input)
     print('\nAOC')
-    print(f"Solution 1: {str(solutions[0])} in {solutions[2][1]-solutions[2][0]:.4f}s")
-    print(f"Solution 2: {str(solutions[1])} in {solutions[2][2]-solutions[2][1]:.4f}s")
-    print(f"\nExecution total: {solutions[2][-1]-solutions[2][0]:.4f} seconds")
+    print(f"Solution 1: {str(solutions[0])} in {solutions[1][1]-solutions[1][0]:.4f}s")
