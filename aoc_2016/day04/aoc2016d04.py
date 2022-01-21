2aa30fd15b157e0116526cb54bf8fadccaba9823
Alex L <alexlong26@gmail.com>
2022-01-21 11:42:26 +0000

    aoc2016d04 complete

--- aoc_2016/day04/aoc2016d04.py
+++ aoc_2016/day04/aoc2016d04.py
@@ -9,8 +9,8 @@ from tkinter.tix import Tree
 from typing import Counter
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / 'input.txt'  # 158835
-input_test = script_path / 'test.txt'  # 1514 / 
+input = script_path / 'input.txt'  # 158835 / 993
+input_test = script_path / 'test.txt'  # 1514 / -
 
 
 def parse(puzzle_input):
@@ -21,47 +21,66 @@ def parse(puzzle_input):
         for l in lines:
             a, checksum = l[:-1].split('[')
             breakup = a.split('-')
-            encrypt_name = ''.join(breakup[:-1])
+            encrypt_name = ' '.join(breakup[:-1])
             sector = int(breakup[-1])
-            # tally = dict(Counter(encrypt_name.replace('-','')).most_common())
             data.append((encrypt_name, sector, checksum)) 
 
     return data
 
 
-def part1(data):
-    """Solve part 1""" 
-
-    valid_sectors = []
-
-    for room in data:
+def validate_rooms(rooms):
+    valid_rooms = []
+    
+    for room in rooms:
         (name, sector, checksum) = tuple(room)
-        # print(name)
-        # print(sector)
-        # print(checksum)
-
-        # list of Tuples: letter & count, in order. If same value, listed in order found
-        tally = Counter(name).most_common()  
-        # resort this using lambda function to return altered tuple
-        #    1st: descending count (using -m[1])
-        #    2nd: alphabetical (using m[0])
+        strippedname = name.replace(' ','')
+        tally = Counter(strippedname).most_common()  
         tally = sorted(tally, key=lambda m: (-m[1],m[0]))
-
-        # Just take the first 5 most common and form a check sequence
         seq = ''.join([str(x) for x, _ in tally[:5]])
         
         if seq == checksum:
-            valid_sectors.append(sector)
+            valid_rooms.append((name, sector))
+    
+    return valid_rooms
 
-    print("Valid Room sectors:", valid_sectors)
 
+def part1(data):
+    """Solve part 1""" 
+
+    # Refactors to put ina validate rooms function to reuse for Part 2
+    valid_sectors = [id for _, id in validate_rooms(data)]
     return sum(valid_sectors)
 
 
 def part2(data):
     """Solve part 2"""
-    
-    return 1
+
+    target_room = 'northpole object storage'
+    decrypted_rooms = []
+
+    for room in validate_rooms(data):
+        name, cipher = room
+        converted_room = []
+        cycle = cipher % 26
+        
+        for c in name:
+            if c == ' ':
+                converted_room.append(c)
+                continue
+            
+            if (ord(c) + cycle) <= ord('z'):
+                new = ord(c) + cycle
+            else:
+                new = ord(c) + cycle - ord('z') + ord('a') - 1
+
+            converted_room.append(chr(new))
+
+        decrypted_rooms.append((''.join(converted_room), cipher))
+
+    # print(decrypted_rooms)
+    filter_object = filter(lambda a: target_room == a[0], decrypted_rooms)
+
+    return list(filter_object)
  
 
 def solve(puzzle_input):
