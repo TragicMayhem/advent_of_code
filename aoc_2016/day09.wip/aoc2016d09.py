701a0f9ba40fbddd1834ac6c0257babc81b79c23
Alex L <alexlong26@gmail.com>
2022-01-27 21:18:12 +0000

    aoc thinking

--- aoc_2016/day09.wip/aoc2016d09.py
+++ aoc_2016/day09.wip/aoc2016d09.py
@@ -14,18 +14,17 @@ def parse(puzzle_input):
     """Parse input """
     with open(puzzle_input, 'r') as file:
         data = file.read()
-        print(data)
+        # print(data)
     return data
 
 
 def part1(data):
     """Solve part 1""" 
     
-    current_position = 0
     compressed_data = data
+    decompressed_sections = []
 
-    look_ahead = marker_pattern.search(compressed_data)
-
+    # look_ahead = marker_pattern.search(compressed_data)
     # print(look_ahead.groups())
     # print(look_ahead.group())
     # print(look_ahead.span())
@@ -37,40 +36,35 @@ def part1(data):
     0 7
     192 7'''
 
-    decompressed_sections = []
-
     # while current_position < len(compressed_data):
     while len(compressed_data):
 
         look_ahead = marker_pattern.search(compressed_data)
 
         if look_ahead:
-            print()
-            print(look_ahead.group(), look_ahead.span() )
             marker_length, marker_qty = (int(x) for x in look_ahead.groups())
             match_start, match_end = look_ahead.span()
-            print('length:', marker_length, 'qty:', marker_qty)
-            print('start:', match_start, 'end:', match_end)
+            print(look_ahead.group(), '  \t', look_ahead.span() )
+            # print('length:', marker_length, 'qty:', marker_qty)
+            # print('start:', match_start, 'end:', match_end)
 
             before = compressed_data[:match_start]
             after = compressed_data[match_end:]
 
-            print('before:', before)
-            print('after :', len(after), '##10', after[:10], '##-20', after[-20:])
+            # print('before:', before)
+            # print('after :', len(after), '##10', after[:10], '##-20', after[-20:])
 
             focus_characters = after[:marker_length]
-            print('Focus chars', len(focus_characters))
-            # print(focus_characters)
+            # print('Focus chars', len(focus_characters))
             
             if len(before): decompressed_sections.append(before)
             
             decompressed_part = focus_characters * marker_qty
-            print('decompressed part len:', len(decompressed_part))
-
             decompressed_sections.append(decompressed_part)
-
             compressed_data = after[marker_length:]
-            print('len compressed:', len(compressed_data), 'first 15:', compressed_data[0:15])
+
+            # print('decompressed part len:', len(decompressed_part))
+            # print('len compressed:', len(compressed_data), 'first 15:', compressed_data[0:15])
 
             # break
 
@@ -82,9 +76,82 @@ def part1(data):
     full_decompressed_length = sum(len(x) for x in decompressed_sections)
     print()
     print('how many decompressed data sections?', len(decompressed_sections))
-    print('Sum lengths:', full_decompressed_length)
+    print('Decompressed length:', full_decompressed_length)
     
     return full_decompressed_length
+
+
+def decompress_data1(compressed_input):
+
+    look_ahead = marker_pattern.search(compressed_input)
+
+    # need to take account for string BEFORE match??
+    # If the compressed input has no instructions, then process that string and return the length
+    if not look_ahead:
+        return len(compressed_input) ##???
+
+    if look_ahead:
+        marker_length, marker_qty = (int(x) for x in look_ahead.groups())
+        match_start, match_end = look_ahead.span()
+        print(look_ahead.group(), '  \t', look_ahead.span(), '\t')
+
+        before = compressed_input[:match_start]
+        after = compressed_input[match_end:]
+        print(compressed_input[match_end:match_end+marker_length])
+
+        found = marker_pattern.findall(compressed_input[match_end:match_end+marker_length])
+        if found:
+            print("more")
+
+            for item in found:
+                print(item)
+
+        # base case is what - nothing found, so return the length of that base string up, multiple by qty
+        # but what about adding the others to it... how to know?
+        # return marker_qty * decompress_data(after)
+
+
+
+
+
+        # if '(' not in after:
+        #     return marker_length * marker_qty
+
+        # # need to do len(before) + recursive call?
+
+        # # canwe look at () then count ( within the first ones range)
+        # # process and tally then move on
+
+        # remaining_characters = after[:marker_length]
+        
+        # # return decompress_data(remaining_characters)
+
+        # if len(before): decompressed_sections.append(before)
+        # # this means there are more instructions to process. We have to go deeper
+
+
+        # decompressed_part = focus_characters * marker_qty
+        # decompressed_sections.append(decompressed_part)
+        # compressed_data = after[marker_length:]
+
+    return 1
+
+
+def decompress2(data):
+
+    # what if split all by (
+    # build dictionary of the parent childs like a route tree
+    # add up the leaves that have no brackest?
+
+    # seq_len: X   seq_rtp: y  child: {same if brackets} data: string remaining
+    # len of children is
+    # need to sum the children then multiuple by the parent qty
+
+    # or return the new string up. add it to the data  - much too big, point is do by numbers
+
+
+
+    return 88
 
 
 def part2(data):
@@ -93,10 +160,23 @@ def part2(data):
         (3x3)XYZ still becomes XYZXYZXYZ, as the decompressed section contains no markers.
         X(8x2)(3x3)ABCY becomes XABCABCABCABCABCABCY, because the decompressed data from the (8x2) marker is then further decompressed, thus triggering the (3x3) marker twice for a total of six ABC sequences.
         (27x12)(20x12)(13x14)(7x10)(1x12)A decompresses into a string of A repeated 241920 times.
+        
         (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445 characters long.
+                9(3*3)  6(2*3) 10(5*2) X        6(3*2) 35(5*7)
+              -------------------------       ------------------      
+                   25(9+6+10)                      41(6+35)
+          75(25*3)                        369(9*41)
+                  75+369= 444 + 1 char (X) = 445
     """
     
     # 12×12×14×10×12 = 241920
+    # look for first number (x), then x chars after the ).
+    #   if ( ) in that sequence, look again
+    #   decompress and return the tally
+    # recursion?  pass in string left, until terminal and add to next one
+    # repeat
+
+    ans = decompress_data1(data)
 
     return 1
  
