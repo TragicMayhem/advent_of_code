38e049330b1ea211b3a16573695ce0c4f0e09354
Alex L <alexlong26@gmail.com>
2022-12-03 07:25:15 +0000

    day 3 2022

--- aoc_2022/day03/aoc2022d03.py
+++ aoc_2022/day03/aoc2022d03.py
@@ -2,33 +2,73 @@
 
 import pathlib
 import time
+import string
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / "input.txt"
-input_test = script_path / "test.txt"
+input = script_path / "input.txt"  # 7811
+input_test = script_path / "test.txt"  # 157 / 70
+
+alphabet = list(string.ascii_letters)
 
 
 def parse(puzzle_input):
     """Parse input"""
-
+    data = []
     with open(puzzle_input, "r") as file:
-        lst = file.read().split(
-            "\n"
-        )  #  Read each line (split \n) and form a list of strings
+        lst = file.read().split("\n")
 
-    return [int(n) for n in lst]
+    return lst
 
 
-def part1(data):
+def part1(backpacks):
     """Solve part 1"""
 
-    return 1
+    data = []
+    all_common_items = []
+    scores = []
+
+    for pack in backpacks:
+        length = int(len(pack) / 2)
+        comp1 = pack[:length]
+        comp2 = pack[length:]
+        comp1_items = comp1.split()
+        comp2_items = comp2.split()
+        data.append([comp1_items, comp2_items])
+
+    backpacks = data
+
+    for pack in backpacks:
+        comp1, comp2 = pack
+        items1 = set(comp1[0])
+        items2 = set(comp2[0])
+        all_common_items.append(items1 & items2)
+
+    for c in all_common_items:
+        if len(c) == 1:
+            scores.append(alphabet.index(c.pop()) + 1)
+        else:
+            print("more than 1 item:", c)
+
+    return sum(scores)
 
 
-def part2(data):
+def part2(backpacks):
     """Solve part 2"""
 
-    return 2
+    scores = []
+
+    for group_start in range(0, len(backpacks), 3):
+        elf1 = set(backpacks[group_start])
+        elf2 = set(backpacks[group_start + 1])
+        elf3 = set(backpacks[group_start + 2])
+        common = elf1 & elf2 & elf3
+
+        if len(common) == 1:
+            scores.append(alphabet.index(common.pop()) + 1)
+        else:
+            print("more than 1 item:", common)
+
+    return sum(scores)
 
 
 def solve(puzzle_input):
