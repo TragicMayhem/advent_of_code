24548a3fae82ec4f160402c1025b6be9d53130ef
Alex L <alexlong26@gmail.com>
2022-12-04 07:03:07 +0000

    day 4 2-22 done

--- aoc_2022/day04/aoc2022d04.py
+++ aoc_2022/day04/aoc2022d04.py
@@ -4,31 +4,76 @@ import pathlib
 import time
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / "input.txt"
-input_test = script_path / "test.txt"
+input = script_path / "input.txt"  # 524 //   798
+input_test = script_path / "test.txt"  # 2 //
 
 
 def parse(puzzle_input):
     """Parse input"""
 
     with open(puzzle_input, "r") as file:
-        lst = file.read().split(
-            "\n"
-        )  #  Read each line (split \n) and form a list of strings
+        data = file.read().split('\n')  # Read file make list by splitting on new line \n
+        data = [tuple(d.split(",")) for d in data]
+        data = [[tuple(sec.split("-")) for sec in pair] for pair in data]
+        data = [[tuple(map(int,sec)) for sec in pair] for pair in data]
 
-    return [int(n) for n in lst]
+    return data
+
+
+def check_sections_fully_overlap(a,b):
+    checks = []
+    if a[0] <= b[0] and a[1] >= b[1]:
+        checks.append(True)
+
+    if b[0] <= a[0] and b[1] >= a[1]:
+        checks.append(True)
+
+    return any(checks)
+
+
+def check_sections_for_overlap(a,b):
+
+    checks = []
+
+    if a[0] <= b[0] and a[1] >= b[0]:
+        checks.append(True)
+
+    if a[0] <= b[0] and a[1] >= b[1]:
+        checks.append(True)
+
+    if b[0] <= a[0] and b[1] >= a[0]:
+        checks.append(True)
+
+    if b[0] <= a[0] and b[1] >= a[1]:
+        checks.append(True)
+
+    return any(checks)
 
 
 def part1(data):
     """Solve part 1"""
+    overlaps = []
 
-    return 1
+    for pair in data:
+        section1, section2 = pair
+        ans = check_sections_fully_overlap(section1,section2)
+        if ans:
+            overlaps.append(pair)
+
+    return len(overlaps)
 
 
 def part2(data):
     """Solve part 2"""
 
-    return 2
+    overlaps = []
+    for pair in data:
+        section1, section2 = pair
+        ans = check_sections_for_overlap(section1,section2)
+        if ans:
+            overlaps.append(pair)
+            
+    return len(overlaps)
 
 
 def solve(puzzle_input):
