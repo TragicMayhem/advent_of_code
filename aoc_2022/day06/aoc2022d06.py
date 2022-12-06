00d514c34d7dac14bd56b5364b99fb760070ae45
Alex L <alexlong26@gmail.com>
2022-12-06 08:18:05 +0000

    day6 done

--- aoc_2022/day06/aoc2022d06.py
+++ aoc_2022/day06/aoc2022d06.py
@@ -1,34 +1,65 @@
-# https://adventofcode.com/2022/day/6
+# https://adventofcode.com/2022/day/1
 
 import pathlib
 import time
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / "input.txt"
-input_test = script_path / "test.txt"
+input = script_path / "input.txt"  # 1651 //
+input_test = script_path / "test0.txt"  # 7 // 19
+input_test1 = script_path / "test1.txt"  # 5  // 23
+input_test2 = script_path / "test2.txt"  # 6  // 23
+input_test3 = script_path / "test3.txt"  # 10  // 29
+input_test4 = script_path / "test4.txt"  # 11  // 26
 
 
 def parse(puzzle_input):
     """Parse input"""
 
     with open(puzzle_input, "r") as file:
-        lst = file.read().split(
-            "\n"
-        )  #  Read each line (split \n) and form a list of strings
+        lst = file.read().split()
+        lst = list(lst[0])
 
-    return [int(n) for n in lst]
+    return lst
+
+
+def find_marker(data, marker_length):
+
+    for i in range(len(data) - marker_length):
+        current = data[i : i + marker_length]
+        count_chars = set(current)
+        # print(i, current, count_chars)
+        if len(count_chars) == marker_length:
+            break
+
+    return i + len(current)
 
 
 def part1(data):
     """Solve part 1"""
 
-    return 1
+    # for i in range(len(data) - 3):
+    #     current = data[i : i + 4]
+    #     count_chars = set(current)
+    #     if len(count_chars) == 4:
+    #         break
+
+    # ans = i + len(current)
+
+    return find_marker(data, 4)
 
 
 def part2(data):
     """Solve part 2"""
+    # for i in range(len(data) - 14):
+    #     current = data[i : i + 14]
+    #     count_chars = set(current)
+    #     # print(i, current, count_chars)
+    #     if len(count_chars) == 14:
+    #         break
 
-    return 2
+    # ans = i + len(current)
+
+    return find_marker(data, 14)
 
 
 def solve(puzzle_input):
@@ -57,7 +88,19 @@ def runAllTests():
 
     print("Tests")
     a, b = runTest(input_test)
+    print(f"Test.  Part1: {a} Part 2: {b}")
+
+    a, b = runTest(input_test1)
     print(f"Test1.  Part1: {a} Part 2: {b}")
+
+    a, b = runTest(input_test2)
+    print(f"Test2.  Part1: {a} Part 2: {b}")
+
+    a, b = runTest(input_test3)
+    print(f"Test3.  Part1: {a} Part 2: {b}")
+
+    a, b = runTest(input_test4)
+    print(f"Test4.  Part1: {a} Part 2: {b}")
 
 
 if __name__ == "__main__":
