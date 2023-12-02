d1e1983fb5e6e584e106eae549784f09abc6d039
Alex L <alexlong26@gmail.com>
2023-12-02 11:38:42 +0000

    day 2 done v1

--- aoc_2023/day02/aoc2023d02.py
+++ aoc_2023/day02/aoc2023d02.py
@@ -2,32 +2,101 @@
 
 import pathlib
 import time
+from collections import defaultdict
 
 script_path = pathlib.Path(__file__).parent
-input = script_path / "input.txt"  #
-input_test = script_path / "test.txt"  #
+input = script_path / "input.txt"  # 2795 / 
+input_test = script_path / "test.txt"  # 8 / 2286
 
+part1_target = {'red': 12,'green':13,'blue':14}
 
 def parse(puzzle_input):
     """Parse input"""
 
     with open(puzzle_input, "r") as file:
         #  Read each line (split \n) and form a list of strings
-        lst = file.read().split("\n\n")
+        game_records = [d.split(':') for d in file.read().split('\n')]
 
-    return lst
+    game_store = defaultdict(dict)
+
+    for game_info in game_records:
+        game_id = game_info[0]
+        draws = [tuple(p.strip() for p in d.split(',')) for d in game_info[1].split(";")]
+        game_store[game_id] = draws
+        # print(game_id)
+        # print(draws)
+        # print(game_store)
+
+    return game_store
 
 
 def part1(data):
     """Solve part 1"""
+    # print()
+    # print(data)
 
-    return 1
+    valid_games = []
+
+    for k,v in data.items():
+        # print(k,v)
+
+        game_overall = []
+
+        for draws in v:
+            draw_colour_check = []
+
+            for draw in draws:
+                count, colour = draw.split(" ")
+                # print(count, colour)
+
+                if part1_target.get(colour, 0) >= int(count):
+                    draw_colour_check.append(True) 
+                else:
+                    draw_colour_check.append(False)
+                # print(draw_colour_check)
+        
+            if all(draw_colour_check):
+                game_overall.append(True)
+            else:
+                game_overall.append(False)
+
+        if all(game_overall):
+            # print(k, "game ok")
+            valid_games.append(int(k.split(" ")[1]))
+
+
+
+    print()
+    print(valid_games)
+
+
+    return sum(valid_games)
 
 
 def part2(data):
     """Solve part 2"""
+    
+    games_scores = []
+    games_overall_total = 0
 
-    return 1
+    for k,v in data.items():
+        # print(k,v)
+        cube_max = {'red':0, 'blue':0, 'green':0}
+        game_score = 0
+        
+        for draws in v:
+            for draw in draws:
+                count, colour = draw.split(" ")
+                # print(count, colour)
+                count = int(count)  
+                cube_max[colour] = {True: count, False: cube_max.get(colour, 0)} [cube_max.get(colour, 0) < count]
+                
+        game_score = cube_max.get('red',0 ) * cube_max.get('green',0 ) * cube_max.get('blue',0 )
+        games_scores.append(game_score)
+
+    games_overall_total = sum(games_scores)
+
+    return games_overall_total
 
 
 def solve(puzzle_input):
