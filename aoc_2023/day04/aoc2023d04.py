ff300d5c9a7391ac5b4430131c21d513dc580aa4
Alex L <alexlong26@gmail.com>
2023-12-04 07:37:26 +0000

    day4 pt 1 donw

--- aoc_2023/day04/aoc2023d04.py
+++ aoc_2023/day04/aoc2023d04.py
@@ -2,6 +2,7 @@
 
 import pathlib
 import time
+from copy import deepcopy
 
 script_path = pathlib.Path(__file__).parent
 input = script_path / "input.txt"  #
@@ -13,24 +14,60 @@ def parse(puzzle_input):
 
     with open(puzzle_input, "r") as file:
         #  Read each line (split \n) and form a list of strings
-        lst = file.read().split("\n\n")
+        lst = file.read().split("\n")
+        lst = [x.replace(":", "|").replace("  ", " ").split("|") for x in lst]
 
-    return lst
+        cards = []
+        # print(lst)
+
+        for c in lst:
+            card = int(c[0].split(" ")[-1])
+            winners = set(map(int, c[1].strip().split(" ")))
+            nums = set(map(int, c[2].strip().split(" ")))
+            winning_set = nums.intersection(winners)
+            cards.append((card, winners, nums, winning_set))
+
+    return cards
 
 
 def part1(data):
     """Solve part 1"""
 
-    return 1
+    ans = 0
+
+    for d in data:
+        card, winners, nums, winning_set = d
+
+        # winning_set = nums.intersection(winners)
+        if winning_set:
+            ans += 2 ** (len(winning_set) - 1)
+
+    return ans
+
+
+def count_winners():
+    # if winning_set:
+    #     number_cards_won = len(winning_set)
+
+    #     pass
+
+    pass
 
 
 def part2(data):
     """Solve part 2"""
 
+    ans = 0
+
+    working_cards = deepcopy(data)
+
+    for d in working_cards:
+        card, winners, nums, winning_set = d
+
     return 1
 
 
-def solve(puzzle_input):
+def solve(puzzle_input, run="Solution"):
     """Solve the puzzle for the given input"""
     times = []
 
@@ -42,27 +79,17 @@ def solve(puzzle_input):
     solution2 = part2(data)
     times.append(time.perf_counter())
 
+    print(f"{run} 1: {str(solution1)} in {times[1]-times[0]:.4f}s")
+    print(f"{run} 2: {str(solution2)} in {times[2]-times[1]:.4f}s")
+    print(f"\nExecution total: {times[-1]-times[0]:.4f} seconds")
+
     return solution1, solution2, times
 
 
-def runTest(test_file):
-    data = parse(test_file)
-    test_solution1 = part1(data)
-    test_solution2 = part2(data)
-    return test_solution1, test_solution2
-
-
-def runAllTests():
-    print("Tests")
-    a, b = runTest(input_test)
-    print(f"Test1.  Part1: {a} Part 2: {b}")
-
-
 if __name__ == "__main__":
-    runAllTests()
-
-    solutions = solve(input)
     print("\nAOC")
-    print(f"Solution 1: {str(solutions[0])} in {solutions[2][1]-solutions[2][0]:.4f}s")
-    print(f"Solution 2: {str(solutions[1])} in {solutions[2][2]-solutions[2][1]:.4f}s")
-    print(f"\nExecution total: {solutions[2][-1]-solutions[2][0]:.4f} seconds")
+
+    tests = solve(input_test, run="Test")
+
+    print()
+    # solutions = solve(input)
