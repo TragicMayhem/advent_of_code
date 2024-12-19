330affdf5cd2016e19af86bfeed68dd5ab29a6cc
Alex Long <alex.long@johnlewis.co.uk>
2024-12-19 11:08:57 +0000

    2024d19 comple

--- aoc_2024/day19/aoc2024d19.py
+++ aoc_2024/day19/aoc2024d19.py
@@ -4,8 +4,8 @@ import pathlib
 import time
 
 script_path = pathlib.Path(__file__).parent
-soln_file = script_path / "input.txt"  #
-test_file = script_path / "test.txt"  #
+soln_file = script_path / "input.txt"  # 293 / 623924810770264
+test_file = script_path / "test.txt"  # 6 / 16
 
 
 def parse(puzzle_input):
@@ -13,21 +13,82 @@ def parse(puzzle_input):
 
     with open(puzzle_input, "r", encoding="UTF-8") as file:
         #  Read each line (split \n) and form a list of strings
-        lst = file.read().split("\n\n")
+        parts = file.read().split("\n\n")
+        components = parts[0].replace(" ", "").split(",")
+        designs = parts[1].split("\n")
 
-    return lst
+    return (components, designs)
 
+def can_construct_string(target, word_bank):
+    """
+    Determines if a target string can be constructed from a word bank.
+
+    Args:
+        target: The target string to construct.
+        word_bank: A list of words that can be used to construct the target.
+
+    Returns:
+        True if the target string can be constructed, False otherwise.
+    """
+
+    table = [False] * (len(target) + 1)
+    table[0] = True
+
+    for i in range(len(target) + 1):
+        if table[i]:
+            for word in word_bank:
+                if word == target[i:i+len(word)]:
+                    table[i+len(word)] = True
+
+    return table[len(target)]
+
+
+def can_construct_string_count(target, word_bank):
+    """
+    Determines if a target string can be constructed from a word bank and counts the number of ways.
+
+    Args:
+        target: The target string to construct.
+        word_bank: A list of words that can be used to construct the target.
+
+    Returns:
+        The number of ways the target string can be constructed.
+    """
+
+    table = [0] * (len(target) + 1)
+    table[0] = 1  # Empty string can be constructed in 1 way
+
+    for i in range(len(target) + 1):
+        for word in word_bank:
+            if word == target[i:i+len(word)]:
+                table[i+len(word)] += table[i]
+
+    return table[len(target)]
 
 def part1(data):
     """Solve part 1"""
 
-    return 1
+    components, designs = data
+
+    count = 0
+    count2 = 0
+    for d in designs:
+        count += can_construct_string(d, components
+                                      )
+
+    return count
 
 
 def part2(data):
     """Solve part 2"""
+    components, designs = data
 
-    return 1
+    count2 = 0
+    for d in designs:
+        count2 += can_construct_string_count(d,components)
+
+    return count2
+
 
 
 def solve(puzzle_input, run="Solution"):
@@ -51,6 +112,20 @@ def solve(puzzle_input, run="Solution"):
 
 if __name__ == "__main__":
     print("\nAOC")
+
+    bank = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
+    target_strings = ["brwrr", "bggr", "gbbr", "rrbgbr",
+    "ubwu",
+    "bwurrg",
+    "brgr",
+    "bbrgwb"]
+
+    for t in target_strings:
+        if can_construct_string(t, bank):
+            print(f"{t} can be constructed")
+        else:
+            print(f"{t} cannot be constructed")
+
 
     tests = solve(test_file, run="Test")
 
