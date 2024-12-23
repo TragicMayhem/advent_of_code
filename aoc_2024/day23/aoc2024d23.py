73384341d1cd4acb030d06d18e3eb89c3890ab3e
Alex L <alexlong26@gmail.com>
2024-12-23 09:04:29 +0000

    2024d23.2 done

--- aoc_2024/day23/aoc2024d23.py
+++ aoc_2024/day23/aoc2024d23.py
@@ -2,12 +2,21 @@
 
 import pathlib
 import time
+from itertools import combinations
+from collections import defaultdict
+import networkx as nx
 from pprint import pprint as pp
 
 
 script_path = pathlib.Path(__file__).parent
-soln_file = script_path / "input.txt"  # 1344 /
-test_file = script_path / "test.txt"  # 7 /
+soln_file = script_path / "input.txt"  # 1344 / ab,al,cq,cr,da,db,dr,fw,ly,mn,od,py,uh
+test_file = script_path / "test.txt"  # 7 / co,de,ka,ta
+
+# not ab,cr,dr,py,uh
+# not kp,ld,lj,tp,vd
+# not aw,fb,gr,tj,yd
+
+# Had to research modules, networkX to build graphs (need to read up more on this)
 
 
 def parse(puzzle_input):
@@ -35,74 +44,9 @@ def build_connection_dict(connections):
     return connection_dict
 
 
-def find_computer_groups(connection_dict):
-    """
-    Finds groups of computers with at least three members and filters
-    for groups containing at least one computer starting with 't'.
-
-    Args:
-      connection_dict: A dictionary where keys are computer names
-                       and values are lists of connected computers.
-
-    Returns:
-      A list of groups of computers, where each group is a list
-      of computer names.
-    """
-    groups = []
-    for computer, connections in connection_dict.items():
-        if len(connections) >= 2:  # At least two connections for a potential group
-            group = set([computer] + connections)  # Create a set to avoid duplicates
-            if len(group) >= 3 and any(c.startswith("t") for c in group):
-                groups.append(list(group))  # Convert set back to list
-    return groups
-
-
-def find_computer_groups2(connection_dict):
-    """
-    Finds groups of computers with at least three members and filters
-    for groups containing at least one computer starting with 't'.
-
-    Args:
-      connection_dict: A dictionary where keys are computer names
-                       and values are lists of connected computers.
-
-    Returns:
-      A list of groups of computers, where each group is a list
-      of computer names.
-    """
-    groups = set()  # Use a set to avoid duplicate groups
-
-    for computer in connection_dict:
-        # Get all connected computers, including the current computer
-        group = set(connection_dict[computer] + [computer])
-
-        # Add connections to the current computer from other computers
-        for connected in connection_dict[computer]:
-            group.update(connection_dict[connected])
-
-        # Check if the group has at least three members
-        if len(group) >= 3:
-            groups.add(frozenset(group))  # Use frozenset for hashability
-
-    # Filter groups where at least one computer starts with 't'
-    filtered_groups = [
-        list(group) for group in groups if any(c.startswith("t") for c in group)
-    ]
-
-    return filtered_groups
-
-
 def find_computer_groups3(connection_dict):
     """
     Finds groups of three computers where each computer is directly connected to the other two.
-
-    Args:
-      connection_dict: A dictionary where keys are computer names
-                       and values are lists of connected computers.
-
-    Returns:
-      A list of groups of computers, where each group is a list
-      of computer names.
     """
     groups = []
     for computer1, connections1 in connection_dict.items():
@@ -129,27 +73,151 @@ def filter_groups(groups, lett="t"):
     return filtered_groups
 
 
+# works for the test data!
+def find_largest_combined_group(groups, connection_dict):
+    """
+    Finds the largest group of computers where each computer in the group is
+    directly connected to all other computers in the group.
+    """
+    largest_group = []
+
+    pp(groups)
+    for group1 in groups:
+        for group2 in groups:
+            if group1 != group2 and set(group1).intersection(set(group2)):
+                combined_group = list(set(group1) | set(group2))
+                pp(combined_group)
+                if is_fully_connected(combined_group, connection_dict):
+                    if len(combined_group) > len(largest_group):
+                        largest_group = combined_group
+
+    return largest_group
+
+
+# works for the test data!
+def find_largest_combined_groups(groups, connection_dict):
+    """
+    Finds all possible combined groups and returns the largest one.
+
+    Args:
+      groups: A list of groups of three computers, where each group is a list
+              of computer names.
+      connection_dict: A dictionary where keys are computer names
+                       and values are sets of connected computers.
+
+    Returns:
+      A list representing the largest combined group of computers,
+      or an empty list if no such group exists.
+    """
+    all_combined_groups = set()
+
+    # pp(groups)
+    for group1 in groups:
+        for group2 in groups:
+            if group1 != group2 and set(group1).intersection(set(group2)):
+                combined_group = tuple(
+                    sorted(set(group1) | set(group2))
+                )  # Convert to tuple for set membership
+                if is_fully_connected(combined_group, connection_dict) and any(
+                    computer.startswith("t") for computer in combined_group
+                ):
+                    all_combined_groups.add(combined_group)
+
+    pp(all_combined_groups)
+
+    if all_combined_groups:
+        largest_group = max(all_combined_groups, key=len)
+        return list(largest_group)
+    else:
+        return []
+
+
+# def find_largest_combined_groups2(groups, connection_dict):
+#     """
+#     Finds all possible combined groups and returns the largest one.
+#     """
+#     # all_combined_groups = set()
+
+#     # # pp(groups)
+#     # for group1 in groups:
+#     #     for group2 in groups:
+#     #         if group1 != group2 and set(group1).intersection(set(group2)):
+#     #             combined_group = tuple(
+#     #                 sorted(set(group1) | set(group2))
+#     #             )  # Convert to tuple for set membership
+#     #             if is_fully_connected(combined_group, connection_dict):
+#     #                 all_combined_groups.add(combined_group)
+
+#     # pp(all_combined_groups)
+
+#     # if all_combined_groups:
+#     #     largest_group = max(all_combined_groups, key=len)
+#     #     return list(largest_group)
+#     # else:
+#     #     return []
+
+#     # largest_group = []
+#     # for group1 in groups:
+#     #     potential_groups = [group1]
+#     #     for group2 in groups:
+#     #         if group1 != group2 and set(group1).intersection(set(group2)):
+#     #             potential_group = list(set(group1) | set(group2))
+#     #             if is_fully_connected(potential_group, connection_dict):
+#     #                 potential_groups.append(potential_group)
+#     #     if potential_groups and len(max(potential_groups, key=len)) > len(
+#     #         largest_group
+#     #     ):
+#     #         largest_group = max(potential_groups, key=len)
+
+#     return largest_group
+
+
+# def is_fully_connected(group, connection_dict):
+#     """
+#     Checks if all computers in the group are directly connected to each other.
+#     True if all computers in the group are fully connected, False otherwise.
+#     """
+#     for computer1 in group:
+#         for computer2 in group:
+#             if computer1 != computer2 and computer2 not in connection_dict[computer1]:
+#                 return False
+#     return True
+
+
 def part1(data):
     """Solve part 1"""
 
     conn_dict = build_connection_dict(data)
-    print(conn_dict)
-
     grps = find_computer_groups3(conn_dict)
-    pp(grps)
-    print(len(grps))
-
     ans = filter_groups(grps)
-    pp(ans)
-    print(len(ans))
 
-    return 1
+    return len(ans)
 
 
 def part2(data):
     """Solve part 2"""
 
-    return 1
+    # conn_dict = build_connection_dict(data)
+    # all_groups = find_computer_groups3(conn_dict)
+    # print(len(all_groups))
+    # all_groups = filter_groups(all_groups)
+    # print(len(all_groups))
+
+    # largest_group = find_largest_combined_groups2(all_groups, conn_dict)
+    # print("Largest Combined Group:", largest_group)
+    # pwd = sorted(largest_group)
+
+    conn_G = nx.Graph()
+    conn_G.add_edges_from(data)
+
+    cliques_list = list(nx.find_cliques(conn_G))
+    # print(list(nx.find_cliques(conn_G)))
+    print(len(cliques_list))
+
+    largest_group = max(cliques_list, key=len)
+    print(largest_group)
+    pwd = sorted(largest_group)
+    return ",".join(pwd)
 
 
 def solve(puzzle_input, run="Solution"):
